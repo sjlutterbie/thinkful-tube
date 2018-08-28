7f0d33cbcdaf9cab2b8106613cb20b6d2170b423
sjlutterbie <simon.lutterbie@ltrbe.com>
2018-08-28 19:33:03 +0000

    JS requirements outlined in plain English

---
+++ index.js
@@ -0,0 +1,36 @@
+/* TODO: When the user clicks the "Search" button... */
+    /* TODO: Prevent default event from trigginer a page reload */
+    /* TODO: If no search term in the box, alert the user (if necessary) */
+
+    /* TODO: If search term exists... */
+        /* TODO: Clear the search form */
+        /* TODO: Clear existing search results */
+        /* TODO: (If responses slow), load processing gif */
+        /* TODO: Call the search query function */
+
+/* TODO: Query the YouTube API for a list of videos matching the provided search term */
+    /* TODO: Validate/format the search query string (if necessary) */
+    /* TODO: Format the search query */
+    /* TODO: Execute the search query */
+    /* TODO: Return the results */
+    
+    
+/* TODO: When a search result is return, render the search results */
+    /* TODO: Show the "Search results" header, if hidden */
+
+    /* TODO: Process the search query result, summarizing the overall contents */
+        /* TODO: Number of results, pev/next links, etc. */
+    
+    /* TODO: Extract the relevant data from a YouTube API search result */
+    
+    /* TODO: Render the search result HTML for a given result */
+    
+    /* TODO: Update the prev/next navigation buttons */
+        /* TODO: If page=1, hide "prev" button */
+        /* TODO: If page=last(?), hide "next" button */
+        
+/* TODO: When the user clicks on a video thumbnail... */
+    /* TODO: Play the video in a lightbox */
+    
+/* TODO: When the user clicks outside the lightbox... */
+    /* Make the video stop and the lightbox disappear */
