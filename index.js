b87ee076c776fa7a6c22d1359e3c2f07d12f3234
sjlutterbie <simon.lutterbie@ltrbe.com>
2018-08-28 19:53:11 +0000

    Implemented search form event handler

--- index.js
+++ index.js
@@ -1,18 +1,56 @@
-/* TODO: When the user clicks the "Search" button... */
-    /* TODO: Prevent default event from trigginer a page reload */
-    /* TODO: If no search term in the box, alert the user (if necessary) */
+//When a user clicks on the "Search" button...
 
-    /* TODO: If search term exists... */
-        /* TODO: Clear the search form */
-        /* TODO: Clear existing search results */
+function searchFormEventHandler() {
+    $('main').on('click', '.js-search-submit', function(e) {
+        e.preventDefault()
+    
+        // Store the search term
+        let searchTerm = $('#searchTerm').val()
+    
+        // If search term doesn't exist...
+        if (!searchTerm) {
+            //Alert the user
+            alert("Search term required!");
+            
+            //Exit the function
+            return;
+        }
+    
+        // If search term exists...
+    
+            //Clear the search form
+            $('#searchTerm').val('');
+    
+            //Clear search results
+            clearSearchResults();
+            
+            // Call the search query function
+            queryYouTubeAPI(searchTerm);
+        
+    });
+}
+
+function clearSearchResults() {
+    // Clears the search results display and hides the prev/next nav buttons
+
+    //Clear existing search results
+    $('.js-search-results').html('');
+    
+    //Hide search navigation links
+    $('.js-search-nav-prev').hide();
+    $('.js-search-nav-next').hide();
+        
+}
+
+function queryYouTubeAPI(searchTerm) {
+    /* TODO: Query the YouTube API for a list of videos matching the provided search term */
         /* TODO: (If responses slow), load processing gif */
-        /* TODO: Call the search query function */
+        /* TODO: Validate/format the search query string (if necessary) */
+        /* TODO: Format the search query */
+        /* TODO: Execute the search query */
+        /* TODO: Return the results */
 
-/* TODO: Query the YouTube API for a list of videos matching the provided search term */
-    /* TODO: Validate/format the search query string (if necessary) */
-    /* TODO: Format the search query */
-    /* TODO: Execute the search query */
-    /* TODO: Return the results */
+}
     
     
 /* TODO: When a search result is return, render the search results */
@@ -34,3 +72,11 @@
     
 /* TODO: When the user clicks outside the lightbox... */
     /* Make the video stop and the lightbox disappear */
+    
+function handleSearchApp() {
+    //Load event handlers
+    searchFormEventHandler();
+}
+
+/* TODO: App Launch Code! */
+$(handleSearchApp);
